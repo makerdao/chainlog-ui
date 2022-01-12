9f0cfe102303934f26b407b8205f920de1615dba
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-01-12 18:43:59 -0500

    feat: call, to_ascii

--- api.py
+++ api.py
@@ -2,8 +2,9 @@
 
 import os
 import requests
+import json
 
-def get_version(chain):
+def call(chain, calldata):
     infura_key = os.environ["INFURA_KEY"]
     endpoint = "https://{}.infura.io/v3/{}".format(chain, infura_key)
     response = requests.post(endpoint, json={
@@ -11,7 +12,7 @@ def get_version(chain):
         "method": "eth_call",
         "params": [{
             "to": "0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F",
-            "data": "0x54fd4d50"
+            "data": calldata
         }, "latest"],
         "id": 0
     })
@@ -20,13 +21,41 @@ def get_version(chain):
         print(result)
         exit()
     data = result["result"]
+    return data
+
+def to_ascii(hex_string):
+    bytes32_string = bytes.fromhex(hex_string)
+    bytes_string = bytes32_string.replace(b"\x00", b"")
+    ascii_string = bytes_string.decode("ascii")
+    return ascii_string
+
+def get_version(chain):
+    version_signature = "0x54fd4d50"
+    data = call(chain, version_signature)
     version_hex = data[2 * (1 + 2 * 32):]
-    version_bytes32 = bytes.fromhex(version_hex)
-    version_bytes = version_bytes32.replace(b"\x00", b"")
-    version = version_bytes.decode("ascii")
+    version = to_ascii(version_hex)
     return version
+
+def get_log(chain):
+    list_signature = "0x0f560cd7";
+    data = call(chain, list_signature)
+    length_hex = data[2 * (1 + 32) : 2 * (1 + 32 + 32)]
+    length = int(length_hex, 16)
+    names = data[2 * (1 + 2 * 32):]
+    if length != len(names) / 64:
+        print("error:", data)
+        exit()
+    for i in range(0, len(names), 64):
+        name_hex = names[i : i + 64]
+        name = to_ascii(name_hex)
+        print(name)
 
 chain = "mainnet"
 version = get_version(chain)
 path = "{}/{}.json".format(chain, version)
-print(os.path.exists(path))
+if not os.path.exists(path):
+   file = open(path, "w")
+   log = get_log(chain)
+   json.dump(log, file)
+
+get_log(chain)
