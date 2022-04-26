538ca57839f41c520434c6f36d8acda23c8a5b64
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-04-26 09:57:30 -0400

    feat: checksum service

---
+++ checksum.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from eth_utils import is_address, to_checksum_address
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        content = self.path[1:]
+        if is_address(content):
+            checksum = to_checksum_address(content)
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(bytes(checksum, "utf-8"))
+        else:
+            self.send_response(400)
+            self.end_headers()
+
+server = HTTPServer(("localhost", 8080), Handler)
+server.serve_forever()
