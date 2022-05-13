b6d0c74426dcc55a7d2d18363d0282361d1b0493
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-05-13 11:24:30 -0400

    fix: allow CORS for 400 responses

--- checksum.py
+++ checksum.py
@@ -7,13 +7,14 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         content = self.path[1:]
         if is_address(content):
-            checksum = to_checksum_address(content)
             self.send_response(200)
             self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
+            checksum = to_checksum_address(content)
             self.wfile.write(bytes(checksum, "utf-8"))
         else:
             self.send_response(400)
+            self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
 
 server = HTTPServer(("0.0.0.0", 8080), Handler)
